de747fb9d682065cc75b285b8e6f33b17b469f52
Nino Schelcher <onin1998@gmail.com>
2020-07-01 19:12:09 +0200

    first styling and mongoose schema

--- models/model.js
+++ models/model.js
@@ -1,1 +1,21 @@
-//Mongoose model is gonna be here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let User = new Schema({
+    email: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    age: Number,
+    gender: String,
+    lookingfor: String,
+    profilepicture: String,
+    hobbies: [String],
+    liked: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    disliked: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    matches: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+})
+
+const UserModel = mongoose.model('User', User);
+
+module.exports = UserModel;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const sassMiddleware = require('node-sass-middleware');
 const router = require('./src/router.js');
 
